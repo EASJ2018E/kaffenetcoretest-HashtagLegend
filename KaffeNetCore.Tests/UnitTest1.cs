d553475b091ae2ad720d08f352c6fcf6a0b9527e
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-09-11 19:04:35 +0200

    3/7 unit tests done

--- KaffeNetCore.Tests/UnitTest1.cs
+++ KaffeNetCore.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using System;
+using kaffe;
+using KaffeNetCore.kaffe;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KaffeNetCore.Tests
@@ -6,8 +9,60 @@ namespace KaffeNetCore.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAfPrisIkkeUnder0()
         {
+            //arrange
+            var sortKaffe = new SortKaffe(20);
+
+            //act
+
+            int pris = sortKaffe.Pris();
+
+            //assert
+
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestAfCortadoMælk()
+        {
+            //arrange
+            var  cortadoKaffe = new Cortado();
+            //act
+            int mlkMælk = cortadoKaffe.MlMælk();
+            //assert
+            Assert.AreEqual(25, mlkMælk);
+        }
+
+        [TestMethod]
+        public void TestAfCortadoPris()
+        {
+            //arrange
+            var cortadoKaffe = new Cortado();
+            //act
+            int pris = cortadoKaffe.Pris();
+            //assert
+            Assert.AreEqual(25, pris);
+        }
+
+        [TestMethod]
+        public void FlatwhiteStyrke()
+        {
+            //arrange
+            var flatWhite = new FlatWhite();
+            //act
+            string styrke = FlatwhiteStyrke("mild");
+            //assert
+            Assert.AreEqual(styrke, "mild");
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            //arrange
+            //act
+            //assert
         }
     }
 }
